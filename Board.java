205fab7df73a9532d083997f2240e4dd1174b249
Betty <zhangbh0605@gmail.com>
2018-01-30 18:40:17 -0700

    kill me, Betty

---
+++ Board.java
@@ -0,0 +1,18 @@
+/**
+* created January 30, 2018
+* @author Betty Zhang
+* Blueprint for ship objects
+*/
+
+class Board{
+	int [][] gameBoard;
+	
+	public Board(int size){
+		gameBoard = new int [size][size];
+		
+	}
+	
+	public void placeShips(){
+		//comment
+	}
+}
