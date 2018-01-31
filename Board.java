837be5c3cda2a21cc0ca00f2a93fe24772fc6835
Brandon Lu <lubrandon@protonmail.com>
2018-01-31 10:26:06 -0700

    Added returnBoard and createBoard - Brandon

--- Board.java
+++ Board.java
@@ -1,3 +1,5 @@
+// make grid look pretty like matrix
+import java.util.StringJoiner;
 /**
 * created January 30, 2018
 * @author Betty Zhang
@@ -7,9 +9,19 @@
 class Board{
 	int [][] gameBoard;
 	
-	public Board(int size){
+	public void createBoard(int size){
 		gameBoard = new int [size][size];
 		
+	}
+
+	public void returnBoard() {
+		for (int[] row : gameBoard) {
+            StringJoiner split = new StringJoiner(" | ");
+			for (int column : row) {
+				split.add(String.format("%01d", column));
+			}
+			System.out.println(split.toString());
+		}
 	}
 	
 	public void placeShips(){
--- Game.java
+++ Game.java
@@ -1,6 +1,13 @@
 public class Game{
 
+    public static void main(String[] args) {
+        int boardSize = 8;
+        Board gameMap = new Board();
+        gameMap.createBoard(8);
 
+        gameMap.returnBoard();
+        
+    }
 
 
 }
