daf8781e5842a81a45313a6b367699b70cdc615c
Betty <zhangbh0605@gmail.com>
2018-01-30 18:05:26 -0700

    creating classes, Betty

---
+++ Ship.java
@@ -0,0 +1,21 @@
+/**
+* created January 30, 2018
+* @author Betty Zhang
+* Blueprint for ship objects
+*/
+
+class Ship{
+	int id;
+	int length;
+	boolean hit = false;
+	boolean sunken = false;
+	
+	public Ship(int num, int len){
+		id = num;
+		length = len;
+	}
+	
+	
+	
+	
+}
