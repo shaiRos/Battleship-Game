958b62ebcb97e8bb45ee2d1e6be4b66d1f7cf7d3
Charlene <charlene.madayag@ucalgary.ca>
2018-02-08 17:58:10 -0700

    New readfile code, reads by line

--- Updated Files FEB-8/ReadTest.java
+++ Updated Files FEB-8/ReadTest.java
@@ -4,32 +4,68 @@ import java.util.Scanner;
 public class ReadTest{
 	public static void main(String[] args)
 	{
-		String fileToRead = "readmebitch.txt";
-        int shipInfo = 0;
-        char[] cbuf = new char[5];
-        int off = 0;
-        int len = 2;
-        
+		getShipInfo();
+    }
+    
+
+    public static void getShipInfo()
+    {
+        //Initiate which file to read
+        String fileToRead = "readmebitch.txt";
+
+        //Initiate line for ship data from file 
+        String shipInfo = null;
+
 
 		try {
-            // FileReader reads text files in the default encoding.
+            // FileReader reads text files
             FileReader fileReader = new FileReader(fileToRead);
 
-            // Wrap FileReader in BufferedReader.
+            // Wrap FileReader in BufferedReader to efficiently read chars, lines, etc. (lines in this case)
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-            //Reads array and prints each integer 
-            while((shipInfo = bufferedReader.read(cbuf, off, len)) != 0) {
-                if (shipInfo == -1) {
-                   System.out.println("End of ship information.");
-                   break;
+            //Initiate array that stores ship info 
+            int shipArray[][] = new int[4][4];
 
-                }
-                else {
-                    System.out.println(cbuf);
-                }
-        	}
+            //Initiate counter for shipArray
+            int shipCounter = 0;
 
+            //Extracting data from file
+            while ((shipInfo = bufferedReader.readLine()) != null) {
+               
+                /* Spliting line by spaces. Depending on how we want to display the data we can 
+                ** determine how to split info
+                */
+                String[] line = shipInfo.split(" ");
+
+                //Stores ship data in seperate variables 
+                int orientation = Integer.parseInt(line[0]);
+                int length = Integer.parseInt(line[1]);
+                int column = Integer.parseInt(line[2]);
+                int row = Integer.parseInt(line[3]);
+
+                //Stores ship data in an array that will soon be stored into a 2D array
+                int[] info = {orientation,length,column,row};
+
+                //Extracting data, storing into array
+                shipArray[shipCounter] = info;
+
+                //Ship data check
+                System.out.println("Ship " + shipCounter + ": " + orientation + " " +
+                         length + " " + column + " " + row + " ");
+                shipCounter++;
+
+            }
+
+            /*Error checking in shipArray info
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                    System.out.println(shipArray[i][j]) ;
+            }
+            */
+
+            //Need to always close after done using it
             bufferedReader.close();         
         }
         catch(FileNotFoundException ex) {
