a7416137979ae62a8faa686e076dad0e4469244a
Shaina <35936758+shainamae98@users.noreply.github.com>
2018-02-09 15:03:36 -0700

    Added comments

--- Updated Files/GameConfig.java
+++ Updated Files/GameConfig.java
@@ -10,11 +10,13 @@ public class GameConfig {
 		boolean formatted = false;
 		while (formatted != true){
 			try {
-				
+				//asd for input for the variables needed to place ships
 				System.out.print("\nIndicate (orientation length column row): ");
 				Scanner Setup = new Scanner(System.in);
 				String setup = Setup.nextLine();
+				//take the input that was converted into String and separate the info
 				String setupInfo[] = setup.split(" ");
+				//store info to designated variables and convert string to their types
 				char orientation = setupInfo[0].toLowerCase().charAt(0);
 				int length = Integer.parseInt(setupInfo[1]);
 				char tempColumn = setupInfo[2].toUpperCase().charAt(0);
@@ -22,24 +24,25 @@ public class GameConfig {
 				int row = Integer.parseInt(setupInfo[3]);
 
 				//all checks
-				validateCoordinate(row,column, board.getBoardSize());		//check if coordinates are within the board
-				validateShipProperties(board,length,orientation);
-				board.shipFitsBoard(orientation,length,row,column);
+				validateCoordinate(row,column, board.getBoardSize());	//check if coordinates are within the board
+				validateShipProperties(board,length,orientation);	//checks if ship properties meet the rules of the game
+				board.shipFitsBoard(orientation,length,row,column);	//check if ship fits board depending on coordinates 
 				
 				// Adds ship to the grid
 				board.addShip(orientation,length,row,column);
 				//sets the values of the ship object
 				name.setShipValues(orientation,length,row,column); 				
-				
-				
 				formatted = true;
 				
 			}
 			catch (NumberFormatException | StringIndexOutOfBoundsException | ArrayIndexOutOfBoundsException e) {
+				//possible errors when doing the conversions of the string input
 				System.out.println("Wrong format");
 				formatted = false;
 			}
 			catch (IllegalArgumentException e) {
+				//input must meet the requirements. This is done in the validate methods. If it doesn't,the methods throws this
+				//exception, exits the loop, and asks the user for a new value that meets the requiremnts.
 				System.out.println(e.getMessage());
 				formatted = false;				
 			
@@ -47,20 +50,21 @@ public class GameConfig {
 		}
 	}
 	
-	//method for choosing row or column indicate "row" or "column" in params.
+	//validates coordinates are reasonable depending on board size
 	public static void validateCoordinate(int xCoord, int yCoord, int boardSize) {
 		if ((xCoord > boardSize || xCoord < 0) || (yCoord > boardSize || yCoord < 0)) { //check if coordinates are within the board
+			//throws error so it doesn't continue the procedures in the setUpInput exception. since input doesn't meet requirements
 			throw new IllegalArgumentException("\ncoordinate over the board size");
 		}
 		
 	}
-	
+	//validates ship properties are reasonable depending on game rules
 	public static void validateShipProperties(Board board, int len, char orientation) {
 		
 		if (len > Board.getMaxShipSize() || len < Board.getMinShipSize()) {  //check if ship is the supported size
 			throw new IllegalArgumentException("Ship size is not supported");
 		}
-		if (orientation != 'h' && orientation != 'v') {
+		if (orientation != 'h' && orientation != 'v') { //Only takes choices between horizontal or vertical.
 			throw new IllegalArgumentException("\nPlease indicate h or v");	
 		}
 	}
