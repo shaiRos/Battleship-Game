4515cc4994c3b4e420064749b864d46341717728
Betty <zhangbh0605@gmail.com>
2018-02-09 17:28:48 -0700

    ship class, Betty

--- Updated Files/Ship.java
+++ Updated Files/Ship.java
@@ -5,10 +5,10 @@
 */
 
 class Ship{
-	int length = 0;
-	char orientation = 0;
-	int column = 0;
-	int row = 0;
+	private int length = 0;
+	private char orientation = 0;
+	private int column = 0;
+	private int row = 0;
 
 	// constructor for ship
 	public Ship(char orient,int len, int col, int ro){
@@ -20,37 +20,15 @@ class Ship{
 		// parts of the ship have and haven't been hit. Makes checking if it's sunket alot easier too.
 		int[] shipPoints = new int [len];
 	}
+	
 
-	public void setCoordinates(int x, int y, char orientation){
-		int [][] coordinates;
-		coordinates = new int [length][2];
-		
-		for(int i=0; i<length; i++){
-			for(int j = 0; j < coordinates[i].length; j++){
-				switch (orientation){
-					case 'v':
-						coordinates[i][j] = x+1;
-						break;
-					case 'h':
-						coordinates[i][j] = y+1;
-						break;
-					default:
-						System.out.println("Oops. Something broke. This should never happen.");
-				}
-			}
-		}
-		/*code to determine coordinates of the ship (you can decide 
-		*how you want to do it and change the parameters
-		*/
-		
-	}
 
 	public void setShipValues(char orient, int len, int col, int ro){
 		orientation = orient;
 		length = len;
 		column = col;
 		row = ro;	
-	}	
+	}
 
 	
 }
