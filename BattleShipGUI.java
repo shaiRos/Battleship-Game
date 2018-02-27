da73cbfb16260acafef3bcfd515488bdcc5e7701
Shaina <shainamae98@gmail.com>
2018-02-26 21:29:21 -0700

    added mini board ~ Shaina

--- BattleShipGUI.java
+++ BattleShipGUI.java
@@ -5,20 +5,20 @@ import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.TilePane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.geometry.Insets;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.RowConstraints;
+
+
 
 public class BattleShipGUI extends Application
 {
 	private int windowSize = 500;
-	
+	private int gridSize = 10; //Default size
+	//GridSize: 20 max for  WindowSize: 500 , increase window size if want a bigger gridSize  
 	
 	public static void main(String [] args)
 	{
@@ -33,13 +33,12 @@ public class BattleShipGUI extends Application
 		Scene gameUI = new Scene(uiLayout, windowSize,windowSize);
 		
 		
-		//uiLayout.setTop(topPanel());		//don't think top and left panel is needed
+
 		uiLayout.setBottom(botPanel());
 		uiLayout.setCenter(battleField());	
-		//uiLayout.setLeft(leftPanel());
 		uiLayout.setRight(rightPanel());
-
-
+		//uiLayout.setPadding(new Insets(5));
+		
 		primaryStage.setTitle("BattleShip");
 		primaryStage.setScene(gameUI);
 		primaryStage.show();
@@ -49,37 +48,42 @@ public class BattleShipGUI extends Application
 	//using gridPanes since children can span multiple col or rows
 	public GridPane battleField() {	
 		
-		int gridSize = 9;
+		int Array[][] = new int [gridSize][gridSize];
+		String object;
 		
-		GridPane battleField = new GridPane();
+		//create the GridPane object for guess board
+		BoardGUI battleField = new BoardGUI(gridSize);
+		//adding values from a 2d array
+		battleField.addValuesFromArray(Array);
 		
-		for (int y = 0; y < gridSize; y++) {
-			//this sets the constraints for box size so the size doesn't automatically adjust to child inside
-			ColumnConstraints column = new ColumnConstraints();
-			RowConstraints row = new RowConstraints();			
-			
-			//so it fits the parent slot (center) for the grid whatever the size of the grid
-			column.setPercentWidth(50);			
-			row.setPercentHeight(50);			
-			
-			battleField.getColumnConstraints().add(column);			
-			battleField.getRowConstraints().add(row);
-			
-			//ONLY FOR DEBUG. find another way to display grid lines. was too dark anyways...	
-			battleField.setGridLinesVisible(true);
-			
-			/* For adding stuff in the grid
-			for (int x = 0; x < gridSize; x++) {
-				
-			//grid isn't formed until you put something in it...
-				Label label = new Label("wee");		
-				battleField.add(label,y,x);	
-			}	*/			
-		}	
-		
-		return battleField;
+		return battleField.getBoardGrid();
 	}
+	
+	
+	public HBox botPanel() {
+		
+		
+		HBox botPanel = new HBox();   
+		botPanel.setStyle("-fx-background-color: #CC6600;");	//Hex color		
+		
+		//make GridPane for own board
+		BoardGUI ownBoard = new BoardGUI(gridSize);		
+		botPanel.getChildren().add(ownBoard.getBoardGrid());	
 
+		return botPanel;
+	}
+	
+	
+	public VBox rightPanel() {
+		
+		VBox rightPanel = new VBox(10);
+		rightPanel.setPadding(new Insets(60));
+        rightPanel.setStyle("-fx-background-color: #0066CC;");	
+		
+		return rightPanel;
+	}	
+	
+	
 	public HBox topPanel() {
 		
 		HBox topPanel = new HBox();	    
@@ -88,31 +92,17 @@ public class BattleShipGUI extends Application
 		return topPanel;
 	}
 
-	public GridPane botPanel() {
-		
-		GridPane botPanel = new GridPane();
-		botPanel.setGridLinesVisible(true);
-		botPanel.setPadding(new Insets(50));     
-		botPanel.setStyle("-fx-background-color: red;");			
-		
-		return botPanel;
-	}
+
 	
 	public VBox leftPanel() {
 		
 		VBox leftPanel = new VBox(10);
-		leftPanel.setPadding(new Insets(50));
+		leftPanel.setPadding(new Insets(100));
         leftPanel.setStyle("-fx-background-color: blue;");			
 		return leftPanel;
 	}	
 	
-	public VBox rightPanel() {
-		
-		VBox rightPanel = new VBox(10);
-		rightPanel.setPadding(new Insets(50));
-        rightPanel.setStyle("-fx-background-color: blue;");		
-		return rightPanel;
-	}
+
 }	
 	
 	
---
+++ BoardGUI.java
@@ -0,0 +1,56 @@
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.control.Label;
+import javafx.geometry.Insets;
+
+public class BoardGUI {
+	
+	GridPane board;
+	int gridSize;
+	
+	//constructor for making the GridPane Board
+	public BoardGUI(int gridsize) {
+		
+		gridSize = gridsize;
+		board = new GridPane();
+		board.setPadding(new Insets(5)); //margin for the slot the grid will be in
+		board.setPrefSize(100,100);		
+		
+		for (int x = 0; x < gridSize; x++) {
+			//this sets the constraints for box size so the size doesn't automatically adjust to child inside
+			ColumnConstraints column = new ColumnConstraints();
+			RowConstraints row = new RowConstraints();			
+			
+			//so it fits the parent slot (center) for the grid whatever the size of the grid
+			column.setPercentWidth(50);			
+			row.setPercentHeight(50);			
+			
+			board.getColumnConstraints().add(column);			
+			board.getRowConstraints().add(row);
+			
+			//ONLY FOR DEBUG. find another way to display grid lines. was too dark anyways...	
+			board.setGridLinesVisible(true);
+		}
+	}
+	
+	//returns the GridPane object
+	public GridPane getBoardGrid() {
+		return board;
+	}
+	
+	//method still usable if objects is not in an array.
+	//Adding objects into the grid is similar when doing it on 2d arrays...		
+	public void addValuesFromArray(int[][] boardArray) {
+		
+		for (int x = 0; x < gridSize; x++) {
+			for (int y = 0; y < gridSize; y++) {
+				//add the object to this coordinate
+				String object = Integer.toString(boardArray[y][x]);
+				Label label = new Label(object);		
+				board.add(label,x,y);	
+			}
+		}
+	}		
+		
+}	
