9838f66d50d20c4d11607a3b708f3181fb61680f
Shaina <shainamae98@gmail.com>
2018-03-02 21:29:42 -0700

    Player turns? kinda...

--- GameGUI/AttackClickHandler.java
+++ GameGUI/AttackClickHandler.java
@@ -2,17 +2,24 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
-
-
-
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
 
 public class AttackClickHandler implements EventHandler<MouseEvent> {
 	
 	double blockSize;
 	int x;
 	int y;
+	Scene scne;
+	BoardGUI nextPlayerOwnBoard;
+	BoardGUI nextPlayerGuessBoard;
 	
-	public AttackClickHandler(BoardGUI grid) {
+	public AttackClickHandler(BoardGUI grid, Scene stage) {
+		
+		nextPlayerGuessBoard = new BoardGUI(10, 770);
+		nextPlayerOwnBoard = new BoardGUI(10, 250);
+		scne = stage;
 		blockSize = grid.getGridBlockSize();
 	}
 	
@@ -21,5 +28,24 @@ public class AttackClickHandler implements EventHandler<MouseEvent> {
 		x = (int)((myEvent.getX()-10)/(blockSize))+1;
 		y = (int)((myEvent.getY()-10)/blockSize)+1;
 		System.out.println(x + ", " + y);
+		
+		Ship ship1 = new Ship('h', 5, 1, 1);
+		Ship ship2 = new Ship('v', 3, 3, 3);			
+		nextPlayerOwnBoard.setupBoardFromShipObjects(ship1);
+		nextPlayerOwnBoard.setupBoardFromShipObjects(ship2);	
+
+		
+		
+		
+		
+		BaseGameLayout testUI = new BaseGameLayout(scne, nextPlayerOwnBoard, nextPlayerGuessBoard);
+		//Scene testScene = new Scene(testUI.BaseGameUI(), 1040, 920);
+		
+		
+		scne.setRoot(testUI.BaseGameUI());
+
+		//
+		
+		
 	}
 }
--- GameGUI/BaseGameLayout.java
+++ GameGUI/BaseGameLayout.java
@@ -1,98 +1,72 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.Group;
-import javafx.stage.Stage;
-import javafx.scene.layout.VBox;
-import javafx.scene.layout.HBox;
+
 import javafx.scene.layout.BorderPane;
+import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.TilePane;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
 import javafx.geometry.Insets;
-import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 
 
-
-
-public class BattleShipGUI extends Application
-{
+public class BaseGameLayout {
 	
-	//DON'T TOUCH 	
 	final private int xWindowSize = 1040;
 	final private int yWindowSize = 920;
 	final private int botHeight = 150;
 	final private int smallGridWidth = 250; //including margins
 	final private int rightPanelWidth = 270;	
-	final private int bigGridWidth = 770; //including margins	
+	final private int bigGridWidth = 770; //including margins		
 	
-
 	private int gridSize = 10; //max 20
 	private BoardGUI guessBoard;
 	private BoardGUI ownBoard;
 
-	private Stage primaryStage;
 	private Scene gameUI;	
-	private BorderPane uiLayout;
+
 	private SidePane sidePane;
-	private BaseGameLayout testUI;
+	private BorderPane uiLayout;
 	
-	//private Scene testScene;
 	
-	public static void main(String [] args)
-	{
-		launch(args);
+	
+	public BaseGameLayout(Scene scene, BoardGUI ownboard, BoardGUI guessboard) {
+		
+		ownBoard = ownboard;
+		guessBoard = guessboard;
+		gameUI = scene;
+	
 	}
-								
-	@Override
-	public void start(Stage primaryStage) throws Exception
-	{
-		
-		primaryStage = primaryStage;
+	
+	
+	
+	
+	public BorderPane BaseGameUI() {
+	
 		uiLayout = new BorderPane();
-		//testUI = new BaseGameLayout();
-		gameUI = new Scene(uiLayout, xWindowSize, yWindowSize);
-		//testScene = new Scene(testUI.BaseGameUI(), xWindowSize, yWindowSize);
-		
 		uiLayout.setCenter(battleField());	
 		uiLayout.setBottom(botPanel());		
-		uiLayout.setRight(rightPanel());
-
-		//primaryStage.setResizable(false);			
-		primaryStage.setTitle("BattleShip");
-		primaryStage.setScene(gameUI);
-		primaryStage.show();
+		uiLayout.setRight(rightPanel());	
+		return uiLayout;
 	}
-	
 
 	
 	
+	
+		
 	//using gridPanes since children can span multiple col or rows
 	public TilePane battleField() {	
 		
 		TilePane centerSlot = new TilePane();
-		int Array[][] = new int [gridSize][gridSize];
-		Array[0][0] = 5; //WIP laying out one ship image for consecutive values of 5..
-		Array[0][1] = 5; //from 2d Array
-		Array[0][2] = 5; 
-		//centerSlot.setPrefTileWidth(770);
-		guessBoard = new BoardGUI(gridSize, bigGridWidth); 
-
-		//had it take a ship object to setup from
-		//this should be changed into taking a ship array instead of individual ship objects
-		Ship ship1 = new Ship('h', 5, 1, 1);
-		Ship ship2 = new Ship('v', 3, 3, 3);			
-		guessBoard.setupBoardFromShipObjects(ship1);
-		guessBoard.setupBoardFromShipObjects(ship2);		
+		
 		
 		guessBoard.getBoardGrid().setOnMousePressed(new AttackClickHandler(guessBoard, gameUI));
 		centerSlot.getChildren().add(guessBoard.getBoardGrid());
 
 		return centerSlot; 	
 	}
+
 	
 	public TilePane rightPanel() {
 		
@@ -100,8 +74,11 @@ public class BattleShipGUI extends Application
 		rightPanel.setPrefWidth(rightPanelWidth);
         rightPanel.setStyle("-fx-background-color: #0066CC;");	
 		rightPanel.setPadding(new Insets(10));	
+			
 		
-		ownBoard = new BoardGUI(gridSize, smallGridWidth);		
+
+		
+		
 		rightPanel.getChildren().add(ownBoard.getBoardGrid());			
 		return rightPanel;
 	}	
@@ -136,6 +113,4 @@ public class BattleShipGUI extends Application
 	}	
 	
 
-}	
-	
-	
+}			
