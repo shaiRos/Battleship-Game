e8a555ef1ceb6e1be2991a448a505551ddc9aae0
Shaina <shainamae98@gmail.com>
2018-03-16 18:04:10 -0600

    Temp submission folder for demo

---
+++ BattleShip Demo 3/Settings.java
@@ -0,0 +1,16 @@
+/**
+*	Settings for the game window
+*/
+
+public class Settings {
+
+	//Window Settings
+	final public static int xWindowSize = 1040;
+	final public static int yWindowSize = 920;
+	final public static int botHeight = 150;
+	final public static int smallGridWidth = 250; //including margins
+	final public static int sidePanelWidth = 270;	
+	final public static int bigGridWidth = 770; //including margins	
+	
+}
+	
