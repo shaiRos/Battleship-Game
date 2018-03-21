37c7f68becb8713dde42b19e932c34c888959b50
Betty <zhangbh0605@gmail.com>
2018-03-21 10:06:52 -0600

    reverting

--- Game.java
+++ Game.java
@@ -270,7 +270,7 @@ public class Game{
 			// Send the attack to the board once properly formatted
 			System.out.println("row " + row1 + "column " + column1);
 			GameConfig.sendAttack(player1Board,row1,column1);
-			shipSunk = GameConfig. mj(player2Board,row1,column1);
+			shipSunk = GameConfig. checkSunken(player2Board,row1,column1);
 
 
             // Check for remaining ships on enemy board
