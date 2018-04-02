c83496660f119397c71e5f77d97d4bcc3f458a75
Betty <zhangbh0605@gmail.com>
2018-04-02 08:59:19 -0600

    comment and minor changes

--- manualProject/game/Game.java
+++ manualProject/game/Game.java
@@ -16,15 +16,19 @@ import java.io.*;
  * 
  * @author Brandon Lu, Shaina Rossel, Betty Zhang, Charlene Madayang
  *
- **/
+ */
 public class Game {
 	// This will toggle if our game will let us fight another player or an AI
 	private static boolean aiStatus = false;
 	private static boolean hitSuccess = false;
 	private static boolean shipSunk = false;
 
+	/**
+	* default constructor to create a game object
+	* default to player vs. player mode if called
+	*/
 	public Game() {
-
+		start();
 	}
 
 	/**
@@ -34,7 +38,7 @@ public class Game {
 	 * @param specifyAIStatus
 	 *            - Boolean that signifies whether the game will implement Player vs
 	 *            Player or Player vs AI
-	 **/
+	 */
 	public Game(boolean specifyAIStatus) {
 		aiStatus = specifyAIStatus;
 		start();
@@ -43,7 +47,7 @@ public class Game {
 	/**
 	 * A toggle that will set the flag which enables the AI
 	 *
-	 **/
+	 */
 	public static void enableAI() {
 		aiStatus = true;
 	}
@@ -52,7 +56,7 @@ public class Game {
 	 * A getter that returns the AI flag's status
 	 * 
 	 * @return aiStatus - Boolean that when true, indicates the AI has been selected
-	 **/
+	 */
 	public static boolean getAIStatus() {
 		return aiStatus;
 	}
@@ -65,7 +69,10 @@ public class Game {
 	public static boolean getHitSuccess() {
 		return hitSuccess;
 	}
-
+	
+	/**
+	* @Brandon what does this dod
+	*/
 	public static void setHitSuccess(boolean b) {
 		hitSuccess = b;
 	}
@@ -74,7 +81,7 @@ public class Game {
 	 * When ran on unix systems, will clear the console for improved output and
 	 * management of the text version of the game
 	 *
-	 **/
+	 */
 	// https://stackoverflow.com/questions/2979383/java-clear-the-console
 	public static void clearScreen() {
 		// ASCII escape codes
@@ -88,8 +95,7 @@ public class Game {
 	 * 
 	 * @param milliseconds
 	 *            - Int of milliseconds to pause execution
-	 **/
-
+	 */
 	public static void sleepThread(int milliseconds) {
 		// Try sleeping for specified time given in ms
 		try {
@@ -108,7 +114,7 @@ public class Game {
 	 *            - Board object that stores all of the information of the main
 	 *            player's board player2Board - Board object that stores all of the
 	 *            information of the opposing player's board
-	 **/
+	 */
 	public static void setupBoard(Board player1Board, Player player1, Board player2Board, Player player2) {
 
 		System.out.println("Player 1 setup phase: ");
@@ -133,6 +139,11 @@ public class Game {
 
 	}
 	
+	/**
+	* the main function used for the players to setup their ship placements
+	* @param playerBoard the Board object of the player setting the ship
+	* @param currentPlayer the current player
+	*/
 	public static void userPlaceShip(Board playerBoard, Player currentPlayer){
 		int [] typeOfShipToAdd = Board.generateShipsToAdd();
 		int maxShips = typeOfShipToAdd.length; // max number of ships for each board
@@ -160,7 +171,7 @@ public class Game {
 	 *            - Ship object that will be used to store all information about the
 	 *            player's respective ships board - Holds all of the information and
 	 *            game state of the current board
-	 **/
+	 */
 	// The main code for inserting ships on the other board
 	// Error checking, logic checking etc
 	public static void setupInput(Board board, Player currentPlayer, int shipLength, int shipCount){
@@ -168,7 +179,7 @@ public class Game {
 
 		while (formatted != true) {
 			try {
-				System.out.println("Placing a length " + shipLength);
+				System.out.println("\nPlacing a length " + shipLength + " ship");
 				String setup = currentPlayer.playerSetup();
 				// take the input that was converted into String and separate the info
 				String setupInfo[] = setup.split(" ");
@@ -187,7 +198,7 @@ public class Game {
 			} catch (NumberFormatException | StringIndexOutOfBoundsException | ArrayIndexOutOfBoundsException e) {
 				// possible errors when doing the conversions of the string input
 				if(currentPlayer instanceof HumanPlayer){
-					System.out.println("Wrong format, example: h A 1");
+					System.out.println("\nWrong format, example: h A 1");
 				}
 				formatted = false;
 			} catch (IllegalArgumentException e) {
@@ -209,7 +220,7 @@ public class Game {
 	 *            - The board the win condition scan will be performed on
 	 * @return boolean - Will return a boolean to indicate whether the win
 	 *         conditions have been met
-	 **/
+	 */
 	// Check the board for remaining ships
 	public static boolean winCondition(Board board) {
 		int shipCounter = 0;
@@ -235,8 +246,9 @@ public class Game {
 	 * @param mapLevel
 	 *            - The final that contains the information required to build the
 	 *            level
+	 * @param board the board being setup
 	 *
-	 **/
+	 */
 	public static void mapFromFiles(String mapLevel, Board board) {
 
 		// Initiate line for ship data from file
@@ -286,19 +298,16 @@ public class Game {
 	/**
 	 * Default board difficulties Use the AI thingy to setup random board placement
 	 * Implement Ship class features - ship sunk Fix board size constants
-	 **/
-	/**
 	 * Starting method that will instantiate all of our variables and begin the game
 	 * loop
-	 *
-	 **/
+	 */
 	public void start() {
 		// create boards for both the players
 		// difficulty will rely on these settings - add user input to specify difficulty
 		int userBoardSize = 5;
 		int userShipCount = 2;
+		boolean winCondition = false;
 
-		String fileName = "map.txt";
 
 		// Initialize the boards and set the board sizes
 		// WIP:
@@ -308,10 +317,9 @@ public class Game {
 		Board player2Board = new Board();
 		// populate boards with battleships
 
-		// This will allow user to input coordinates and setup board
-		// setupBoard(player1Board, player2Board, userShipCount);
 
 		// This will read a file and allow us to setup predefined board
+		//String fileName = "map.txt";
 		//mapFromFiles(fileName, player1Board);
 		//mapFromFiles(fileName, player2Board);
 
@@ -322,26 +330,21 @@ public class Game {
 		Player player2 = null;
 
 		// Create a new human that can access their boards
-		/**
+		/*
 		 * Make the player an inheritance of a Player class
-		 **/
+		 */
 		if (getAIStatus() != true) {
 			player2 = new HumanPlayer(player2Board);
 		} else {
 			player2 = new ComputerPlayer(player2Board);
 		}
 
-		// set the win condition
-		boolean winCondition = false;
-
-		// Game loop
-		
+		// This will allow user to input coordinates and setup board
 		setupBoard(player1Board, player1, player2Board, player2);
 
-
+		// Game loop
 		do {
 			// set each player's guess board to the other player's game board
-
 			player1Board.guessBoard = player2Board.gameBoard;
 			player2Board.guessBoard = player1Board.gameBoard;
 
@@ -407,15 +410,15 @@ public class Game {
 			}
 
 			// Check for remaining ships on enemy board
-			if (winCondition(player1Board) == true) {
 				System.out.println("Player 2 has won!");
+			if (winCondition(player1Board) == true) {
 				sleepThread(2500);
 				System.exit(0);
 			}
 			sleepThread(1000);
 
-			// check win conditions maybe make this an exception. throws an exception if
-			// winning conditions are met, catches condition and exits loop.
+			/*check win conditions maybe make this an exception. throws an exception if
+			* winning conditions are met, catches condition and exits loop. */
 
 		} while (winCondition != true);
 
