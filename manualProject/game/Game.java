a1031d94d37574623e0b3299d057e12cf8073f02
betty.zhang <betty.zhang@zone17-wb.cs.ucalgary.ca>
2018-04-03 17:03:32 -0600

    turn based

--- manualProject/game/Game.java
+++ manualProject/game/Game.java
@@ -292,7 +292,31 @@ public class Game {
 		}
 
 	}
+/* 
+	private void takeTurn(Player currentPlayer, Board currentBoard, String userName){
+			clearScreen();
+			System.out.println(userName + " turn starting....");
 
+			// Take coordinates from the player turn as col,row
+			// Convert back to usable values
+			String coord = currentPlayer.playerTurn();
+			String[] coordFormatted = coord.split(",");
+			int row1 = Integer.parseInt(coordFormatted[0]);
+			int column1 = Integer.parseInt(coordFormatted[1]);
+			// Send the attack to the board once properly formatted
+			GameConfig.sendAttack(player1Board, row1, column1);
+			shipSunk = GameConfig.checkSunken(player2Board, row1, column1);
+			// Check for remaining ships on enemy board
+			if (winCondition(player2Board) == true) {
+				System.out.println(userName + " has won!");
+				sleepThread(2500);
+				System.exit(0);
+			}
+
+			sleepThread(1000);
+
+	}
+ */
 	/**
 	 * Default board difficulties Use the AI thingy to setup random board placement
 	 * Implement Ship class features - ship sunk Fix board size constants
@@ -336,13 +360,15 @@ public class Game {
 
 		// This will allow user to input coordinates and setup board
 		setupBoard(player1Board, player1, player2Board, player2);
-
+		int currentPlayerNum = 0;
 		// Game loop
 		do {
 			// set each player's guess board to the other player's game board
 			player1Board.guessBoard = player2Board.gameBoard;
 			player2Board.guessBoard = player1Board.gameBoard;
 
+			Board [] boards = new Board[]{player1Board, player2Board};
+			Player [] playres = new Player[]{player1,player2};
 			// Player 1 turn
 			clearScreen();
 			System.out.println("Player 1 turn starting....");
